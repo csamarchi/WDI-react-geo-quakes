ad7df91e4ff45b95edad9120ebe7e1f7a59f1b9c
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-10-31 16:52:20 -0500

    added marker on map refactered previous Map code

--- src/Earthquakes/index.js
+++ src/Earthquakes/index.js
@@ -5,12 +5,11 @@ class Earthquakes extends Component {
   render() {
     const quakeList = this.props.earthquakes.map((earthquakes, i) => {
       return (
-        <li key={i}>
-        Earthquakes Container <br/>
+        <h5 key={i}>
         Title: {earthquakes.properties.title} <br/>
-        coordinates: {earthquakes.geometry.coordinates} <br/>
+        Coordinates: {earthquakes.geometry.coordinates} <br/>
         Time: {earthquakes.properties.time} <br/>
-        </li>
+        </h5>
       )
     });
     return(
